74e466ce8e24972c7187ed08204a8470f87e10a3
Mehmet Aytuğ <63475375+0DAYtug@users.noreply.github.com>
2022-02-05 03:12:07 +0300

    Add files via upload

---
+++ AntiD4dot/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using dnlib.DotNet;
+using System;
+using System.Threading;
+
+void Main()
+{
+
+    Console.WriteLine("-----------------------------------\n\r" +
+        "De4Dot Protector by @0DAYtug\n\r" +
+        "-----------------------------------\n\r");
+    Console.Write("Enter File Path : ");
+    string dosyaYolu = Console.ReadLine();
+
+    AssemblyDef dosya = AssemblyDef.Load(dosyaYolu);
+    ModuleContext contexmodul = ModuleDefMD.CreateModuleContext();
+    ModuleDefMD modul = ModuleDefMD.Load(dosyaYolu, contexmodul);
+    
+    Random rnd = new Random();
+    InterfaceImpl Interface = new InterfaceImplUser(modul.GlobalType);
+    for (int i = 200; i < 300; i++)
+    {
+        TypeDef typedef = new TypeDefUser("", $"Form{i.ToString()}", modul.CorLibTypes.GetTypeRef("System", "Attribute"));
+        InterfaceImpl interfacefake = new InterfaceImplUser(typedef);
+        modul.Types.Add(typedef);
+        typedef.Interfaces.Add(interfacefake);
+        typedef.Interfaces.Add(Interface);
+    }
+
+    Console.Write("Protected File Name : ");
+    string protecte = Console.ReadLine();
+    modul.Write(protecte + ".exe");
+    Console.WriteLine("\n\r[i] Protected File Created !\n\r");
+    Thread.Sleep(5000);
+    System.Environment.Exit(0);
+}
+
+Main();
